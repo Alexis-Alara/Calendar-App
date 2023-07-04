5fb47c047681f74320871c9d04f12a0ed0dc2fe2
Alexis-Alara <alexis.alaraa@gmail.com>
2023-07-04 17:16:18 -0600

    CalendarEvent component

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -2,7 +2,7 @@ import { Calendar } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 import { addHours } from 'date-fns/esm'
-import { Navbar } from "../"
+import { Navbar, CalendarEvent } from "../"
 import { localizer, getMessages } from '../../helpers'
 
 const events = [{
@@ -44,6 +44,9 @@ export const CalendarPage = () => {
         style={{ height: 'calc( 100vh - 80px )' }}
         messages={ getMessages() }
         eventPropGetter={ eventStyleGetter }
+        components={{
+          event: CalendarEvent
+        }}
       />
 
     </>
